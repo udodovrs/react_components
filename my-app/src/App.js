195cfbecd7f569462efe06019519eb8f45914afe
roman <udodovrs@gmzil.com>
2023-06-09 11:49:39 +0300

    add year and comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-const curentYear = new Date().getFullYear().toString();
 
 export const App = () => {
-		const appToHtml = `<div class="App">
-			<header class="App-header">
-				<img src=${logo} class="App-logo" alt="logo" />
-				<p>
+	return (
+  	<div className="App"> {/*декларативный*/}
+			<header className="App-header"> {/*декларативный*/}
+				<img src={logo} className="App-logo" alt="logo" /> {/*декларативный*/}
+				<p> {/*декларативный*/}
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
-					class="App-link"
+					className="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
-				>
+				> {/*декларативный*/}
 					Learn React
 				</a>
-				<p>
-					${curentYear}
+				<p> {/*декларативный*/}
+					{new Date().getFullYear().toString()} {/*императивный*/}
 				</p>
 			</header>
-		</div>`
-		return appToHtml
+		</div>
+	);
 }
 
