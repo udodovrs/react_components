063a42d443315cdf606e8989f2e62da976799f3f
roman <udodovrs@gmzil.com>
2023-06-09 11:37:23 +0300

    rewritten to JS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+const curentYear = new Date().getFullYear().toString();
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+		const appToHtml = `<div class="App">
+			<header class="App-header">
+				<img src=${logo} class="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
-					className="App-link"
+					class="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
@@ -18,10 +18,10 @@ export const App = () => {
 					Learn React
 				</a>
 				<p>
-					{new Date().getFullYear()}
+					${curentYear}
 				</p>
 			</header>
-		</div>
-	);
+		</div>`
+		return appToHtml
 }
 
